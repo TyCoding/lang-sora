69cdb819c4a8a64337ae244411cd87391d6586fb
tycoding <tycoding@sina.com>
2024-02-26 15:08:00 +0800

    optimize static resource

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ const config = {
     // These opinionated rules are enabled in stylistic-type-checked above.
     // Feel free to reconfigure them to your own preference.
     "@typescript-eslint/array-type": "off",
+    '@typescript-eslint/no-explicit-any': 'off',
     "@typescript-eslint/consistent-type-definitions": "off",
 
     "@typescript-eslint/consistent-type-imports": [
